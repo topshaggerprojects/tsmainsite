e54f9606110e30bc4dc8ae610e12c0f1fe288769
Benjamin Hatton <tsprojects@protonmail.com>
2020-04-07 20:28:19 +0100

    7/4/20 20:30

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<a href="index.php"><i class="fa fa-fw fa-home"></i>Home</a>
 	<a href="about.php"><i class="fa fa-fw fa-home"></i>About Us</a>
 	<a href="contact.php"><i class="fa fa-fw fa-home"></i>Contact Us</a>
-	<a href="rottenteachers.php"><i class="fa fa-fw fa-home"></i>Rotten Teachers</a>
 </div>
 <div class="main">
 	<h1> Topshagger HQ </h1><br>

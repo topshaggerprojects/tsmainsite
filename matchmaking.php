5a3c51cc859cd2773235c30d695461729a3ba77d
Benjamin Hatton <tsprojects@protonmail.com>
2020-04-11 22:00:07 +0100

    matchmaking1

--- matchmaking.php
+++ matchmaking.php
@@ -1,13 +1,16 @@
 <html>
+<head>
+</head>
 <body>
 <link rel="stylesheet" href="style.css">
 <div class="sidebar">
 	<a href="index.php"><i class="fa fa-fw fa-home"></i>Home</a>
 	<a href="about.php"><i class="fa fa-fw fa-home"></i>About Us</a>
 	<a href="contact.php"><i class="fa fa-fw fa-home"></i>Contact Us</a>
-	<a href="projects.php"><i class="fa fa-fw fa-home"></i>Our Projects</a>
+	<a href="matchmaking.php"><i class="fa fa-fw fa-home"></i>Scrim Matchmaker</a>
 </div>
-<div class="main">
-	<h1> Topshagger HQ </h1><br>
+<form>
+	<input type="text" name="player1">
+</form>
 </body>
 </html>
--- welcome.php
+++ welcome.php
@@ -6,6 +6,7 @@
 	<a href="about.php"><i class="fa fa-fw fa-home"></i>About Us</a>
 	<a href="contact.php"><i class="fa fa-fw fa-home"></i>Contact Us</a>
 	<a href="projects.php"><i class="fa fa-fw fa-home"></i>Our Projects</a>
+	<a href="matchmaking.php"><i class="fa fa-fw fa-home"></i>Scrim Matchmaker</a>
 </div>
 <div class="main">
 	<h1> Topshagger HQ </h1><br>

4a004ab9b1dfa55021c32da12640c3031e88c542
Benjamin Hatton <tsprojects@protonmail.com>
2020-04-11 22:09:30 +0100

    matchmaking1

--- matchmaking.php
+++ matchmaking.php
@@ -11,11 +11,16 @@
 </div>
 <div class="main">
 <form>
-	Player 1:<input type="text" name="player1"><br>
-	Player 2:<input type="text" name="player2"><br>
-	Player 3:<input type="text" name="player3"><br>
-	Player 4:<input type="text" name="player4"><br>
-	Player 5:<input type="text" name="player5"><br>
+	 Player 1:<input type="text" name="player1"><br>
+	 Player 2:<input type="text" name="player2"><br>
+	 Player 3:<input type="text" name="player3"><br>
+	 Player 4:<input type="text" name="player4"><br>
+	 Player 5:<input type="text" name="player5"><br>
+	 Player 6:<input type="text" name="player1"><br>
+	 Player 7:<input type="text" name="player2"><br>
+	 Player 8:<input type="text" name="player3"><br>
+	 Player 9:<input type="text" name="player4"><br>
+	Player 10:<input type="text" name="player5"><br>
 	<input type="submit" name="Submit">
 </form>
 </div>
